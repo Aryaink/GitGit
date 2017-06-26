810ee20c471c9bc714d9e18a6c038dadad40dc98
Aryaink <prashant.vikal@yahoo.com>
2017-06-26 12:23:53 +0530

    Added Source Files
    Add library files and composer.json

---
+++ src/Aryaink/GitGit/hello.php
@@ -0,0 +1,18 @@
+<?php
+/** Created by PhpStorm.
+ * User: Prashant Vikal
+ * Email: arya@aryaink.com | prashant.vikal@yahoo.com
+ * Website: http://aryaink.com
+ * Date: 26-Jun-17
+ * Time: 11:17 AM
+ */
+
+namespace Aryaink\GitGit;
+
+class HalloThere
+{
+    public function sayHallo()
+    {
+        return 'Hallo!';
+    }
+}
